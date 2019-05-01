062793ea506d8a100390018917b68aac20d80adc
ShiryaevNikolay <deendeeps@list.ru>
2019-05-01 15:08:50 +0300

    Created a list calculator.

--- src/Calculator.c
+++ src/Calculator.c
@@ -1,320 +1,209 @@
 /*
  ============================================================================
- Name        : Calculator.c
- Author      : Shiryaev Nikolay
- Version     :
+ Name        : Structure.c
+ Author      : Nikolay Shiryaev
+ Version     : List(1.0)
  Copyright   : Your copyright notice
- Description : Calculator in C, Ansi-style
+ Description : Calculator(structures) in C
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-float regular_calculator_keyboard() {
-	float a, b;
-	int f = 0;
-	printf("Введите первое число: ");
-	scanf("%f", &a);
+typedef struct element {
+	char oper;
+	char vect;
+	int size;
+	float *value1;
+	float *value2;
+	struct element *next;
+} element;
 
-	int k;
-	puts("\nВыберите операцию:    1) \"+\" - сумма\n    2) \"-\" - разность\n    3) \"*\" - умножение\n    4) \"/\" - деление\n    5) \"!\" - факториал\n    6) \"^\" - степень");
-	printf("Выберите команду: ");
-	scanf("%d", &k);
-	while(f == 0) {
-		if(k > 0 && k < 7)
-			f = 1;
-		else {
-			printf("Нет такой команды. Введите команду из списка: ");
-			scanf("%d", &k);
-		}
+element* getLast(element *head){	// Finding the last item
+	if(head == NULL){
+		return NULL;
 	}
-	if(k != 5) {
-		printf("Введите второе число: ");
-		scanf("%f", &b);
+	while(head->next){
+		head = head->next;
 	}
-
-	float c;
-	switch(k) {
-		case 1:
-			printf("\nРезультат: %.2f\n\n", a + b);
-			break;
-		case 2:
-			printf("\nРезультат: %.2f\n\n", a - b);
-			break;
-		case 3:;
-			printf("\nРезультат: %.2f\n\n", a * b);
-			break;
-		case 4:
-			printf("\nРезультат: %.2f\n\n", a / b);
-			break;
-		case 5:
-			c = 1;
-			for(int i = 1; i <= a; i++) c = c * i;
-			printf("\nРезультат: %.2f\n\n", c);
-			break;
-		case 6:
-			c = a;
-			if(b != 0){
-				for(int i = 1; i < b; i++) {
-					a = a * c;
-				}
-			}
-			else {
-				a = 1;
-			}
-			printf("\nРезультат: %.2f\n\n", a);
-			break;
-	}
-
-	return EXIT_SUCCESS;
+	return head;
 }
 
-float vector_calculator_keyboard() {
-	float *vector1, *vector2;
-	int size, f;
-
-	printf("Введите размерность вектора: ");
-	scanf("%d", &size);
-
-	vector1 = calloc(size,sizeof(float));
-	for (int i = 0; i < size; i++) {
-		printf("Введите координаты первого вектора(a): ");
-		scanf("%f", &vector1[i]);
-	}
-
-	vector2 = calloc(size,sizeof(float));
-
-	for (int i = 0; i < size; i++) {
-		printf("Введите координаты второго вектора(b): ");
-		scanf("%f", &vector2[i]);
-	}
-
-	int k;
-	puts("\nВыберите операцию:    1) \"+\" - сумма\n    2) \"-\" - разность\n    3) \"*\" - скалярное произведение");
-	printf("Выберите команду: ");
-	scanf("%d", &k);
-
-	f = 0;
-	while(f == 0) {
-		if(k > 0 && k < 4){
-			f = 1;
+void pushBack(element *head, FILE *in_file){	// Adding a item to end of list
+	while(!feof(in_file)){
+		element *last = getLast(head);
+		element *tmp = (element*)malloc(sizeof(element));
+		fscanf(in_file, " %c %c", &tmp->oper, &tmp->vect);
+		if(tmp->vect == 'v') {
+			fscanf(in_file, "%d", &tmp->size);
+			tmp->value1 = malloc(tmp->size*sizeof(float));
+			tmp->value2 = malloc(tmp->size*sizeof(float));
+			for(int i = 0; i < tmp->size; i++){
+				fscanf(in_file, "%f", &tmp->value1[i]);
+			}
+			for(int i = 0; i < tmp->size; i++){
+				fscanf(in_file, "%f", &tmp->value2[i]);
+			}
+		}else{
+			tmp->value1 = malloc(sizeof(float));
+			tmp->value2 = malloc(sizeof(float));
+			fscanf(in_file, "%f %f", tmp->value1, tmp->value2);
 		}
-		else {
-			printf("Нет такой команды! Введите команду из списка: ");
-			scanf("%d", &k);
-		}
+		tmp->next = NULL;
+		last->next = tmp;
 	}
-
-	float result = 0;
-	switch(k) {
-		case 1:
-			printf("\nРезультат: (");
-			for (int i = 0; i < size; i++) {
-				printf("%.2f", vector1[i] + vector2[i]);
-				if (i != size-1) printf(" ");
-			}
-			printf(")\n\n");
-			break;
-		case 2:
-			printf("\nРезультат: (");
-			for (int i = 0; i < size; i++) {
-				printf("%.2f", vector1[i] - vector2[i]);
-				if (i != size-1) printf(" ");
-			}
-			printf(")\n\n");
-			break;
-		case 3:
-			for (int i = 0; i < size; i++) {
-				result += vector1[i] * vector2[i];
-			}
-			printf("\nРезультат: %.2f\n\n", result);
-			break;
-	}
-	free(vector1);
-	free(vector2);
-
-	return EXIT_SUCCESS;
 }
 
-float regular_calculator_file(FILE *in_file, FILE *out_file, char k) {
-	float *a = NULL, *b = NULL;
+void printList(const element *head){	// Printing the list to console
+	while(head){
+		printf("%c %c", head->oper, head->vect);
+		if(head->vect == 'v'){
+			printf(" %d", head->size);
+			for(int i = 0; i < head->size; i++) printf(" %.0f", head->value1[i]);
+			for(int i = 0; i < head->size; i++) printf(" %.0f", head->value2[i]);
+		}else{
+			printf(" %.0f %.0f", head->value1[0], head->value2[0]);
+		}
+		head = head->next;
+		printf("\n");
+	}
+}
 
-	a = calloc(1, sizeof(float));
-	b = calloc(1, sizeof(float));
-
-	fscanf(in_file, "%f", a);
-	fscanf(in_file, "%f", b);
-
+void regular_calculator_file(FILE *out_file, element *head) {
 	float c;
-	switch(k) {
+	switch(head->oper) {
 		case '+':
-			fprintf(out_file, "Результат: %.2f", (*a) + (*b));
+			fprintf(out_file, "Result: %.1f", head->value1[0] + head->value2[0]);
 			break;
 		case '-':
-			fprintf(out_file, "Результат: %.2f", (*a) - (*b));
+			fprintf(out_file, "Result: %.1f", head->value1[0] - head->value2[0]);
 			break;
 		case '*':
-			fprintf(out_file, "Результат: %.2f", (*a) * (*b));
+			fprintf(out_file, "Result: %.1f", head->value1[0] * head->value2[0]);
 			break;
 		case '/':
-			fprintf(out_file, "Результат: %.2f", (*a) / (*b));
+			fprintf(out_file, "Result: %.1f", head->value1[0] / head->value2[0]);
 			break;
 		case '!':
 			c = 1;
-			for(int i = 1; i <= (*a); i++) c = c * i;
-			fprintf(out_file, "Результат: %.2f", c);
+			for(int i = 1; i <= head->value1[0]; i++) c = c * i;
+			fprintf(out_file, "Result: %.1f", c);
 			break;
 		case '^':
-			c = *a;
-			if((*b) != 0){
-				for(int i = 1; i < (*b); i++) {
-					(*a) = (*a) * c;
+			c = head->value1[0];
+			if(head->value2[0] != 0){
+				for(int i = 1; i < head->value2[0]; i++) {
+					head->value1[0] = head->value1[0] * c;
 				}
 			}
 			else {
-				*a = 1;
+				head->value1[0] = 1;
 			}
-			fprintf(out_file, "Результат: %.2f", *a);
+			fprintf(out_file, "Result: %.1f", head->value1[0]);
 			break;
 	}
-
-	free(a);
-	free(b);
-
-	return EXIT_SUCCESS;
 }
 
-float vector_calculator_file(FILE *in_file, FILE *out_file, char k) {
-
-	float *vector1, *vector2;
-	int size;
-
-	fscanf(in_file, " %d", &size);
-
-	vector1 = calloc(size,sizeof(int));
-	vector2 = calloc(size,sizeof(int));
-
-	for (int i = 0; i < size; i++) {
-		fscanf(in_file, "%f", &vector1[i]);
-	}
-
-	for (int i = 0; i < size; i++) {
-		fscanf(in_file, "%f", &vector2[i]);
-	}
-
+void vector_calculator_file(FILE *out_file, element *head) {
 	float result = 0;
-	switch(k) {
+	switch(head->oper) {
 		case '+':
-			fprintf(out_file, "(");
-			for (int i = 0; i < size; i++) {
-				fprintf(out_file, "%.2f", vector1[i]);
-				if (i != size-1) fprintf(out_file, " ");
+			fprintf(out_file, "Result: (");
+			for (int i = 0; i < head->size; i++) {
+				fprintf(out_file, "%.1f", head->value1[i]);
+				if (i != head->size - 1) fprintf(out_file, " ");
 			}
 			fprintf(out_file, ") + (");
-			for (int i = 0; i < size; i++) {
-				fprintf(out_file, "%.2f", vector2[i]);
-				if (i != size-1) fprintf(out_file, " ");
+			for (int i = 0; i < head->size; i++) {
+				fprintf(out_file, "%.1f", head->value2[i]);
+				if (i != head->size - 1) fprintf(out_file, " ");
 			}
 			fprintf(out_file, ") = (");
-			for (int i = 0; i < size; i++) {
-				fprintf(out_file, "%.2f", vector1[i] + vector2[i]);
-				if (i != size-1) fprintf(out_file, " ");
+			for (int i = 0; i < head->size; i++) {
+				fprintf(out_file, "%.1f", head->value1[i] + head->value2[i]);
+				if (i != head->size - 1) fprintf(out_file, " ");
 			}
 			fprintf(out_file, ")");
 			break;
 		case '-':
-			fprintf(out_file, "(");
-			for (int i = 0; i < size; i++) {
-				fprintf(out_file, "%.2f", vector1[i]);
-				if (i != size-1) fprintf(out_file, " ");
+			fprintf(out_file, "Result: (");
+			for (int i = 0; i < head->size; i++) {
+				fprintf(out_file, "%.1f", head->value1[i]);
+				if (i != head->size - 1) fprintf(out_file, " ");
 			}
 			fprintf(out_file, ") - (");
-			for (int i = 0; i < size; i++) {
-				fprintf(out_file, "%.2f", vector2[i]);
-				if (i != size-1) fprintf(out_file, " ");
+			for (int i = 0; i < head->size; i++) {
+				fprintf(out_file, "%.1f", head->value2[i]);
+				if (i != head->size - 1) fprintf(out_file, " ");
 			}
 			fprintf(out_file, ") = (");
-			for (int i = 0; i < size; i++) {
-				fprintf(out_file, "%.2f", vector1[i] - vector2[i]);
-				if (i != size-1) fprintf(out_file, " ");
+			for (int i = 0; i < head->size; i++) {
+				fprintf(out_file, "%.1f", head->value1[i] - head->value2[i]);
+				if (i != head->size - 1) fprintf(out_file, " ");
 			}
 			fprintf(out_file, ")");
 			break;
 		case '*':
-			for (int i = 0; i < size; i++) {
-				result += vector1[i] * vector2[i];
+			for (int i = 0; i < head->size; i++) {
+				result += head->value1[i] * head->value2[i];
 			}
-			fprintf(out_file, "(a, b) = %.2f", result);
+			fprintf(out_file, "Result: (a, b) = %.1f", result);
 			break;
 	}
-
-	free(vector1);
-	free(vector2);
-
-	return EXIT_SUCCESS;
-}
-
-int calc_keyboard() {
-	int exit_from_calculator = 1;
-
-	while (exit_from_calculator == 1) {
-		puts("Введите номер калькулятора.");
-		printf("  1) Работа с обычными числами.\n  2) Работа с векторами.\n");
-		int number, f;
-		f = 0;
-		while (f == 0) {
-			scanf("%d", &number);
-			if (number > 0 && number < 3) f = 1;
-			else {
-				printf("Такой команды нет. Выберите команду из списка:\n");
-				printf("  1) Работа с обычными числами.\n  2) Работа с векторами.\n");
-			}
-		}
-
-		if (number == 1) regular_calculator_keyboard();
-		else vector_calculator_keyboard();
-
-		printf("Продолжить использовать калькулятор? \n1)Да - продолжить\n2)Нет - выйти\n");
-		scanf("%d", &exit_from_calculator);
-	}
-
-	return 0;
-}
-
-int calc_file() {
-	FILE *in_file, *out_file;
-	in_file = fopen("input.txt", "r");
-	out_file = fopen("output.txt", "w");
-
-	char operation, calc;
-
-	fscanf(in_file, "%c %c", &operation, &calc);
-	if (calc == 's') regular_calculator_file(in_file, out_file, operation);
-	else vector_calculator_file(in_file, out_file, operation);
-
-	fclose(in_file);
-	fclose(out_file);
-	return EXIT_SUCCESS;
 }
 
 int main(void) {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	setvbuf(stderr, NULL, _IONBF, 0);
 
-	int k;
-	printf("Как вы хотите использовать калькулятор:\n	1) Ввод с клавиатуры\n	2) Работа с файлами\n");
-	while (1 == 1) {
-		scanf("%d", &k);
-		if (k > 0 && k < 3) break;
-		else
-			printf("Такой команды нет. Введите команду из списка");
+	FILE *in_file, *out_file;
+
+	in_file = fopen("input.txt", "r");
+	out_file = fopen("output.txt", "w");
+
+	element *head; // Creating the first list item
+	head = (element*)malloc(sizeof(element));
+	fscanf(in_file, "%c %c", &head->oper, &head->vect);
+	if(head->vect == 'v') {
+		fscanf(in_file, "%d", &head->size);
+		head->value1 = malloc(head->size*sizeof(float));
+		head->value2 = malloc(head->size*sizeof(float));
+		for(int i = 0; i < head->size; i++){
+			fscanf(in_file, "%f", &head->value1[i]);
+		}
+		for(int i = 0; i < head->size; i++){
+			fscanf(in_file, "%f", &head->value2[i]);
+		}
+	}else{
+		head->value1 = malloc(sizeof(float));
+		head->value2 = malloc(sizeof(float));
+		fscanf(in_file, "%f %f", head->value1, head->value2);
+	}
+	head->next = NULL;
+
+	//=================================================================================
+
+	pushBack(getLast(head), in_file); // Creating a list of commands
+
+	element *out; // Creating a new item so as not to lose the beginning of the list
+	out = head;
+	while(out){
+		if(out->vect == 'v'){	// If the operation with vectors, then calling the calculator for vectors
+			vector_calculator_file(out_file, out);
+		}else{	// If the operation with numbers, then we call the usual calculator
+			regular_calculator_file(out_file, out);
+		}
+		out = out->next; // Moving on the list
+		fprintf(out_file, "\n");
 	}
 
-	if (k == 1) calc_keyboard();
-	else
-		calc_file();
+	//=================================================================================
 
-	printf("EXIT");
-	return 0;
+	fclose(in_file);
+	fclose(out_file);
+
+	//printList(head);	Printing the list to console
+
+	return EXIT_SUCCESS;
 }
