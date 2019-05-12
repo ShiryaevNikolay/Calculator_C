15dd7558577d93189b821ac1a6547772a20dc225
ShiryaevNikolay <deendeeps@list.ru>
2019-05-12 12:06:11 +0300

    Добавил функцию, которая возвращает последний элемент СПИСКА. Переделал функцию pushBack

--- src/Calculator.c
+++ src/Calculator.c
@@ -27,7 +27,13 @@ typedef struct arr_val {
 	struct arr_val *next;
 } arr_val;
 
-list* pushBack(FILE *in_file, char c) {
+list* getLast(list *head) { // Получаем последний элемент списка
+	if (head == NULL) return NULL;
+	while (head->next) head= head->next;
+	return head;
+}
+
+void pushBack(FILE *in_file, char c, list *head) {
 	arr_val *head_arr; // Объявляем список для чисел >=10 и отрицательных чисел
 	arr_val *last_arr;
 	head_arr = (arr_val*) malloc(sizeof(arr_val));
@@ -63,7 +69,9 @@ list* pushBack(FILE *in_file, char c) {
 	list *tmp = (list*) malloc(sizeof(list));
 	tmp->emelement = array;
 	tmp->next = NULL;
-	return tmp;
+	list *last = getLast(head);
+	last->next = tmp;
+	printf("%s", last->emelement);
 }
 
 int main(void) {
@@ -79,18 +87,54 @@ int main(void) {
 	list *head_list;
 	head_list = (list*) malloc(sizeof(list));
 	fscanf(in_file, "%c", &c);
-	head_list = pushBack(in_file, c); // Создание первого элемента списка с числами и операциями
 
-	list *last_list;
-	last_list = (list*) malloc(sizeof(list));
-	last_list = head_list;
+	//=====================================================================================================================================
+	/* Создание первного элемента списка */
+	arr_val *head_arr; // Объявляем список для чисел >=10 и отрицательных чисел
+	arr_val *last_arr;
+	head_arr = (arr_val*) malloc(sizeof(arr_val));
+	last_arr = (arr_val*) malloc(sizeof(arr_val));
+
+	char *array; // Объявляем массив для чисел >=10 и отрицательных чисел
+	int size = 0; // Кол-во символов в одном элементе (число, операция)
+	while (c != ' ') {
+		size++;
+		if (size == 1) { // Если кол-во симоволов элемента =1, то создаём первый элемент списка(голову)
+			head_arr->c = c;
+			head_arr->next = NULL;
+			last_arr = head_arr;
+		} else { // Иначе заполняем список, пока не достигнем 0
+			arr_val *tmp = (arr_val*) malloc(sizeof(arr_val));
+			tmp->c = c;
+			tmp->next = NULL;
+			last_arr->next = tmp;
+			last_arr = tmp;
+		}
+		if (feof(in_file) != 0)
+			break;
+		else
+			fscanf(in_file, "%c", &c);
+	}
+	last_arr = head_arr; // Возвращаемся в начало списка
+	array = malloc(size * sizeof(char));
+	for (int i = 0; i < size; i++) { // Записываем значения из списка в массив
+		array[i] = last_arr->c;
+		last_arr = last_arr->next;
+	}
+	// Присваиваем массив (число, операция) в голову списка
+	list *tmp = (list*) malloc(sizeof(list));
+	tmp->emelement = array;
+	tmp->next = NULL;
+	head_list = tmp;
+	//=====================================================================================================================================
+
 	while (!feof(in_file)) { // Заполняем список числами и операциями
 		fscanf(in_file, "%c", &c);
-		last_list->next = pushBack(in_file, c);
-		last_list = last_list->next;
+		pushBack(in_file, c, getLast(head_list));
 	}
 	fclose(in_file);
 
+	list *last_list = (list*)malloc(sizeof(list));
 	last_list = head_list;
 	stek *head_stek;
 	head_stek = (stek*) malloc(sizeof(stek));
